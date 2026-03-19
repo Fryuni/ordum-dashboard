714ab9e70441dc000b73f7496fa897e60bd76bb5
Luiz Ferraz <luiz@lferraz.com>
2026-03-18 23:40:49 -0300

    Update scripts/preprocess-gamedata.ts

--- scripts/preprocess-gamedata.ts
+++ scripts/preprocess-gamedata.ts
@@ -307,6 +307,6 @@ import codex from "./codex.json";
 export const itemsCodex: Map<string, ItemEntry> = new Map((codex as any).items);
 export const recipesCodex: Map<number, CraftRecipe> = new Map((codex as any).recipes);
 export const extractionsCodex: Map<number, ExtractionRecipe> = new Map((codex as any).extractions);
-export const toolItemsCodex: Map<number, ToolItemEntry> = new Map((codex as any).toolItems ?? []);
+export const toolItemsCodex: Map<number, ToolItemEntry> = new Map((codex as any).toolItems);
 `.trim(),
 );
